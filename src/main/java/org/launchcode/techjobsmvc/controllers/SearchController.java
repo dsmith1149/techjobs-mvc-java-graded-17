04279a7e6f36c00d760359e7fdbb1411139a7cbd
dsmith1149 <dantayvionsmith@gmail.com>
2024-11-13 01:04:03 -0600

    final push

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,16 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -15,13 +22,48 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
 @RequestMapping("search")
 public class SearchController {
 
+    static HashMap<String, String> columnChoices = new HashMap<>();
+    static HashMap<String, Object> tableChoices = new HashMap<>();
+
+    public SearchController () {
+        columnChoices.put("all", "All");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("positionType", "Position Type");
+        columnChoices.put("coreCompetency", "Skill");
+
+        tableChoices.put("employer", JobData.getAllEmployers());
+        tableChoices.put("location", JobData.getAllLocations());
+        tableChoices.put("positionType", JobData.getAllPositionTypes());
+        tableChoices.put("coreCompetency", JobData.getAllCoreCompetency());
+    }
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", new ArrayList<Job>());
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model,
+                                       @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
+
+        ArrayList<Job> jobs;
+        if (searchTerm.equalsIgnoreCase("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        }
+        else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Search Results");
+
+        return "search";
+    }
 
 }
 
